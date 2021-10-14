f4a4cde7d7d2fef22cd2a74a47113fe69cba3dda
jbrighter98 <44352550+jbrighter98@users.noreply.github.com>
2021-10-14 13:50:05 -0400

    Update MR_Equations.py

--- MR_Equations.py
+++ MR_Equations.py
@@ -1,1 +1,30 @@
+class Data():
+    
+    # Formatting rules:   https://matplotlib.org/stable/tutorials/text/mathtext.html
+    #
+    # Write equations like this inside braces of self.equations
+    #
+    #    1 : r'$___INSERT FORMATTED EQUATION HERE___$',
+    #    2 : r'$___INSERT FORMATTED EQUATION HERE___$',
+    #    ...
+    #
+    #
+    # Write the name of the equation coresponding to the same number
+    #
+    #    1 : "Name of Equation 1", 
+    #    2 : "Name of Equation 2",
+    #    ...
+    #
+    # DON'T FORGET COMMAS!!!
+    # THE LAST ONE IN THE LISTS DON'T GET COMMAS
+    
+
+    def __init__(self):
+        self.equations = {
+            
+        }
+
+        self.names = {
+            
+        }
 
