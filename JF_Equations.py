5e23080df5fb042892c644f51f411409562d132d
jfink09 <62917477+jfink09@users.noreply.github.com>
2021-10-15 22:51:41 -0400

    Update JF_Equations.py

--- JF_Equations.py
+++ JF_Equations.py
@@ -22,8 +22,20 @@ class Data():
     def __init__(self):
         self.equations = {
             
+            r'$\tau = \mu \frac{dV}{dy}$'
+            r'$L = N\mathrm{cos}\alpha - A\mathrm{sin}\alpha$'
+            r'$D = N\mathrm{sin}\alpha + A\mathrm{cos}\alpha$'
+            r'$q = \frac{1}{2}\rhoV^{2}$'
+            r'$dp = -g \rho dy$'
+            
         }
 
         self.names = {
+            
+            "Shear Stress",
+            "Lift",
+            "Drag",
+            "Dynamic Pressure",
+            "Hydrostatic Equation"
             
         }
